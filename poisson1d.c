4aea56a257a81f04ab0e744972884aadeb92e12c
Ilana Gat <ibg210@nyu.edu>
2012-04-25 11:59:58 -0400

    now have program solve for outer pressures

--- poisson1d.c
+++ poisson1d.c
@@ -32,7 +32,6 @@ void open_file(){
     while(fgets(line, 80, output2) !=NULL){
         sscanf(line, "%lf",&d1uphi[i]);
         
-        //printf("%f, i=%d \n", d1uphi[i], i);
         i+=1;
     }
     fclose(output2);
@@ -51,11 +50,7 @@ void fill_source(){
             //source[i] = radius[i]*Re*Wo*Wo;
             //source[i] = 0.0;
             source[i] = Pouter;
-        }/*else if(i==1){ //this could be sketchy, not quite sure if this is the right thing to do.
-            source[i] = Re*(d1uphi[i-1]/(radius));
-        }else if(i==P_size){
-            source[i] = 2.0*Re*(Wo*d1uphi[i-1]/(radius));
-        }*/else{
+        }else{
             source[i] = Re*d1uphi[i-1]*d1uphi[i-1]/radius[i];
         }
         printf("source[%d] = %f, radius = %f \n ", i, source[i], radius[i]);
@@ -67,11 +62,55 @@ void fill_source(){
 void finite_difference(){
     int i;
     for(i=0; i<P_size; i++){
-        //double radius = r1 + (i*grid_spacing[0]);
         double value = radius[i+1]*(fabs(source[i+2] - source[i]))/(grid_spacing[0]*Re);
         uphi_new[i] = sqrt(value);
         printf("value = %f, uphinew[%d] = %f \n", value, i, uphi_new[i]);
     }
+
+}
+
+
+
+void sparce(){
+    
+    const int M = 102;
+    const int N = 102;
+    int i;
+    // Declare an MxN matrix which can hold up to three band-diagonals.
+    struct cs_sparse *triplet = cs_spalloc(M, N, 3*N, 1, 1);
+    
+    
+    // Fill the diagonal, and the band above and below the diagonal with some
+    // values.
+    
+    
+    for (i=0; i<M; i++) {
+        double a, e;
+        
+        a = 1.0/grid_spacing[0];
+        e = -a;
+        
+        
+        if(i==0){
+            cs_entry(triplet, i, i, 1.0);
+        }else if(i==(M-1)){
+            cs_entry(triplet, i, i, 1.0);
+        }else{
+            cs_entry(triplet, i, i-1, a);
+            cs_entry(triplet, i, i+1, e);
+        }
+        
+        
+    }
+    
+    
+    // Convert the triplet matrix into compressed column form, and use LU
+    // decomposition to solve the system. Note that the vector 'b' of coefficients
+    // overwritten to contain the solution vector 'x'.
+    struct cs_sparse *matrix = cs_compress(triplet);
+    cs_lusol(0, matrix, source, 1e-12);
+    cs_spfree(triplet);
+    cs_spfree(matrix);
 
 }
 
@@ -85,12 +124,14 @@ int main()
 // -----------------------------------------------------------------------------
 {
     //for a 100x100 array, but have 2 ghost cells in it for bcs
-    const int M = 102;
-    const int N = 102;
+    
     P_size = 100;
-    int i, j;
-    Pinner = 1.0;
-    Pouter = 1.0;
+    int i;
+    //Pinner = 1.0;
+    //Pouter = -10.337;
+    
+    Pinner = 10.0;
+    Pouter = 10.0;
     
     d1uphi = (double*) malloc(P_size*sizeof(double));
     uphi_new = (double*) malloc(P_size*sizeof(double));
@@ -101,8 +142,8 @@ int main()
     
     open_file();
     Re = 1.0;
-    Wi = 0.0;
-    Wo = 5.0;
+    Wi = 10.0;
+    Wo = 0.0;
     r1 = 1.0;
     r2 = 2.0;
     
@@ -112,167 +153,40 @@ int main()
     
     fill_source();
     
-
-    //printf("here \n");
-
-    double *matrix2 = (double*) malloc(M*N*sizeof(double));
-    // Declare an MxN matrix which can hold up to three band-diagonals.
-    struct cs_sparse *triplet = cs_spalloc(M, N, 3*N, 1, 1);
-    
-    //cs_entry(triplet, 0, 0, 1.0); // Fill the corners with the value 1
-    //cs_entry(triplet, M-1, N-1, 1.0);
-    
-    // Fill the diagonal, and the band above and below the diagonal with some
-    // values.
-    
-    
-    int position;
-    for(i=0; i<M; i++){
-        for(j=0; j<N; j++){
-            position = (i*N) + j;
-            matrix2[position] = 0.0;
+    sparce();
+    if(source[0]>source[1] || source[P_size+1]>source[P_size]){
+        if(source[0]>source[1]){
+            Pinner = source[1];
         }
+        
+        if(source[P_size+1]>source[P_size]){
+            Pouter = source[P_size];
+        }
+        fill_source();
+        sparce();
     }
-    
-    matrix2[0] = 1.0;
-    matrix2[(M-1)*N + N-1] = 1.0;
-    
-    //printf("here");
-    for (i=0; i<M; i++) {
-        printf("%d \n", i);
-        double a, b, c, d, e;
-        //double radius = r1 + ((i-1)*grid_spacing[0]);
-        //a = grid_spacing[1]*grid_spacing[1]*radius*(radius - grid_spacing[0]);
-        //b = grid_spacing[0]*grid_spacing[0];
-        //c = -2.0*((grid_spacing[1]*grid_spacing[1]*radius*radius) + (grid_spacing[0]*grid_spacing[0]));
-        //d = b;
-        //e = grid_spacing[1]*grid_spacing[1]*radius*(radius + grid_spacing[0]);
-        //a = (1.0/(grid_spacing[0]*grid_spacing[0])) - (1.0/(radius*grid_spacing[0]));
-        //b = 1.0/(radius*radius*grid_spacing[1]*grid_spacing[1]);
-        //c = -2.0*((1.0/(grid_spacing[0]*grid_spacing[0])) + (1.0/(radius*radius*grid_spacing[1]*grid_spacing[1])));
-        //c = -2.0*(1.0/(grid_spacing[0]*grid_spacing[0]));
-        //d = b;
-        //e = (1.0/(grid_spacing[0]*grid_spacing[0])) +(1.0/(radius*grid_spacing[0]));
-        a = 1.0/grid_spacing[0];
-        e = -a;
-        
-        /*if(i<=(P_size)){
-            cs_entry(triplet,i, 0, a);
-            matrix2[(i*N)] = a;
-        }else{
-            cs_entry(triplet,i, i-P_size,  a);
-            matrix2[(i*N) + (i-P_size)] = a;
-        }
-        
-        if(i>=(M-P_size - 1)){
-            cs_entry(triplet,i, N-1, e);
-            matrix2[(i*N) + N-1] = e;
-        }else{
-            cs_entry(triplet,i, i+P_size,  e);
-            matrix2[(i*N) + (i + P_size)] = e ;
-        }
-        
-        
-        if((i-1)%P_size==0){
-            cs_entry(triplet,i, i+(P_size-1),  b);
-            matrix2[(i*N) + (i + P_size-1)] = b;
-        }else{
-            cs_entry(triplet,i, i-1, b);
-            matrix2[(i*N) + (i-1)] = b;
-        }
-        
-        if(i%P_size==0){
-            cs_entry(triplet,i, i-(P_size - 1), d);
-            matrix2[(i*N) + (i - P_size +1)] = d;
-        }else{
-            cs_entry(triplet,i, i+1, d);
-            matrix2[(i*N) + (i +1)] = d ;
 
-        }*/
-        
-        
-        if(i==0){
-            //cs_entry(triplet, i, i, a);
-            cs_entry(triplet, i, i, 1);
-            //cs_entry(triplet, i, i+1, e+a);
-        }else if(i==(M-1)){
-            cs_entry(triplet, i, i, 1);
-            //cs_entry(triplet, i, i-1, a+e);
-            //cs_entry(triplet, i, i, e);
-        }else{
-            cs_entry(triplet, i, i-1, a);
-            //cs_entry(triplet, i, i, c);
-            cs_entry(triplet, i, i+1, e);
-        }
 
-        //cs_entry(triplet, i, i, c);
-        //matrix2[(i*N) + i] = c;
-    }
-    
-    // Declare and initialize the array of coefficients, 'b'.
-    /*double *b = (double*) malloc(N*sizeof(double));
-    double inner = 15.0;
-    double outer =  -15.0;
-    b[0] = inner;
-    b[N-1] = outer;
-    for (i=1; i<N-1; ++i) {
-        //double radius = r1 + ((i-1)*grid_spacing[0]);
-        //double coeffic = grid_spacing[0]*grid_spacing[0]*grid_spacing[1]*grid_spacing[1]*radius*radius;
-        
-        if((i-1)%P_size==0){
-            b[i] = inner;
-        }else if(i%P_size==0){
-            b[i] = outer;
-        }else{
-            b[i] = 0.0;
-
-        }
-        
-    }*/
-    
-    // Convert the triplet matrix into compressed column form, and use LU
-    // decomposition to solve the system. Note that the vector 'b' of coefficients
-    // overwritten to contain the solution vector 'x'.
-    struct cs_sparse *matrix = cs_compress(triplet);
-    cs_lusol(0, matrix, source, 1e-12);
     // Print the solution vector.
-    output=fopen("trial3.txt", "w");
+    output=fopen("pressure.txt", "w");
 
     for (i=0; i<(P_size+2); i++) {
-        /*for(j=0; j<P_size+1; ++j){
-            position = (i*P_size) + j;
-            //printf(" m[%d] = %f", position, matrix2[position] );
-            
-            if(j==50){fprintf(output, "%+5.4e \n", b[i]);}
-
-        }*/
-        //printf("\n");
         printf("source[%d] = %+5.4e\n", i, source[i]);
         fprintf(output, "%+5.4e \n", source[i]);
     }
     
-    for (i=0; i<N; ++i) {
-        //printf("b[%d] = %+5.4e\n", i, b[i]);
-    }
-    
-    
-    //now want to finite difference the source, which is now the pressure and see if we get u back.
-    //dP/dr = Re*Uphi^2/r
-
     finite_difference();
     
     
     output3=fopen("uphi_new.txt", "w");
     
     for (i=0; i<(P_size); i++) {
-        //printf("\n");
-        //printf("source[%d] = %+5.4e\n", i, source[i]);
+
         fprintf(output3, "%+5.4e \n", uphi_new[i]);
     }
     
     // Clean up memory usage.
-    cs_spfree(triplet);
-    cs_spfree(matrix);
+
     free(source);
     
     return 0;
--- poisson_solver.py
+++ poisson_solver.py
@@ -8,18 +8,27 @@ import math as math
 ##u2 = np.loadtxt('trial2.txt',skiprows=1 , unpack=True)
 uphi_shooting = np.loadtxt('Uphi.txt',skiprows=0 , unpack=True)
 uphi_sparse = np.loadtxt('uphi_new.txt',skiprows=0 , unpack=True)
+pressure = np.loadtxt('pressure.txt',skiprows=0 , unpack=True)
+
+
 radius = []
+radius2 = []
+x = 1.0-0.01
+radius2.append(x)
 r1 = 1.0
 for i in range(len(uphi_shooting)):
-	r2 = r1+ i
+	r2 = r1+ (i*0.01)
 	radius.append(r2)
+	radius2.append(r2)
+y = 2.0+0.01	
+radius2.append(y)
 
 # create a figure window
-fig1 = plt.figure(1, figsize=(10,6))
+fig1 = plt.figure(1, figsize=(10,8))
 # add extra white space between subplots
 # (needed for axes labels)
 fig1.subplots_adjust(wspace=0.3, hspace=0.3)
-ax1 = fig1.add_subplot(1,1,1)
+ax1 = fig1.add_subplot(2,1,1)
 
 
 ##ax1.plot(radius, u, '--b')
@@ -30,14 +39,23 @@ ax1.plot(radius, uphi_sparse, '--b', label = 'sparse')
 
 
 ax1.legend(loc = 'best')
+ax1.set_xlim(0.8, 2.2)
 ax1.grid()
 ax1.set_xlabel('radius')
 ax1.set_ylabel(r'$u_{\phi}(r)$')
 ax1.set_title(r'$u_{\phi}(r)$ vs radius')
 
 
+ax2 = fig1.add_subplot(2,1,2)
+ax2.set_xlim(0.8, 2.2)
+ax2.plot(radius2, pressure, 'b')
+ax2.grid()
+ax2.set_xlabel('radius')
+ax2.set_ylabel('Pressure')
+ax2.set_title('Pressure versus radius')
 
-plt.savefig('shooting_and_sparce')
+##plt.savefig('shooting_and_sparce')
+plt.savefig('pressure_and_velocity')
 
 
 plt.show() 
